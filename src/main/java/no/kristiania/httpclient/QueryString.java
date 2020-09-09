b42c635966d2703438e30436fc232e2463c4043d
hagenh <haakonhagen@live.no>
2020-09-09 13:35:52 +0200

    nesten fixet testen

--- src/main/java/no/kristiania/httpclient/QueryString.java
+++ src/main/java/no/kristiania/httpclient/QueryString.java
@@ -1,14 +1,26 @@
 package no.kristiania.httpclient;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class QueryString {
-    private final String value;
+    //private final String value;
+    //private final String parameterName;
+    private Map<String, String> parameters = new HashMap<>();
 
     public QueryString(String queryString){
-        int equalsPos = queryString.indexOf('=');
-        value = queryString.substring(equalsPos+1);
+
+
+        for(String parameter : queryString.split("&")) {
+            //String parameter = queryString;
+            int equalsPos = queryString.indexOf('=');
+            String value = queryString.substring(equalsPos + 1);
+            String parameterName = parameter.substring(0, equalsPos);
+            parameters.put(parameterName, value);
+        }
     }
 
     public String getParameter(String name) {
-        return value;
+        return parameters.get(name);
     }
 }
